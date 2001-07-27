e5466f276e11d6071b09029cfed6d8638970a557
James Strachan <james.strachan@gmail.com>
2001-07-27 14:41:13 +0000

    Added setContextSupport() to Context

--- src/java/main/org/jaxen/expr/DefaultStep.java
+++ src/java/main/org/jaxen/expr/DefaultStep.java
@@ -16,7 +16,7 @@ import java.util.TreeSet;
 import java.util.List;
 import java.util.Iterator;
 
-abstract class DefaultStep implements Step
+public abstract class DefaultStep implements Step
 {
     private IterableAxis axis;
     private PredicateSet predicates;
