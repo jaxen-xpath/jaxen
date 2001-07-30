39a1d4cb255b7396e190796b91ce216395dfa231
werken <werken@43379f7c-b030-0410-81db-e0b70742847c>
2001-07-30 20:12:48 +0000

    Tweaking demos

---
+++ src/java/samples/Dom4jDemo.java
@@ -0,0 +1,59 @@
+
+import org.dom4j.Document;
+import org.dom4j.io.SAXReader;
+
+import org.jaxen.dom4j.XPath;
+
+import org.saxpath.SAXPathException;
+import org.saxpath.XPathSyntaxException;
+
+import java.util.List;
+import java.util.Iterator;
+
+public class Dom4jDemo 
+{
+    public static void main(String[] args)
+    {
+        if ( args.length != 2 )
+        {
+            System.err.println("usage: Dom4jDemo <document url> <xpath expr>");
+            System.exit( 1 );
+        }
+
+        try
+        {
+            SAXReader reader = new SAXReader();
+            
+            Document doc = reader.read( args[0] );
+            
+            XPath xpath = new XPath( args[1] );
+            
+            List results = xpath.selectNodes( doc );
+
+            Iterator resultIter = results.iterator();
+            
+            System.out.println("Document :: " + args[0] );
+            System.out.println("   XPath :: " + args[1] );
+            System.out.println("");
+            System.out.println("Results" );
+            System.out.println("----------------------------------");
+            
+            while ( resultIter.hasNext() )
+            {
+                System.out.println( resultIter.next() );
+            }
+        }
+        catch (XPathSyntaxException e)
+        {
+            System.err.println( e.getMultilineMessage() );
+        }
+        catch (SAXPathException e)
+        {
+            e.printStackTrace();
+        }
+        catch (Exception e)
+        {
+            e.printStackTrace();
+        }
+    }
+}
