662c17a5e74d3afbcb4176a3dabee27347d5ce4e
dmegginson <dmegginson@43379f7c-b030-0410-81db-e0b70742847c>
2001-08-17 17:44:16 +0000

    Added JavaDoc documentation, including a short code example.

--- trunk/jaxen/src/java/main/org/jaxen/dom/XPath.java
+++ trunk/jaxen/src/java/main/org/jaxen/dom/XPath.java
@@ -1,23 +1,57 @@
+// XPath.java - top-level entry point for DOM XPath matching.
+
 package org.jaxen.dom;
 
 import org.jaxen.Navigator;
 import org.jaxen.BaseXPath;
 import org.saxpath.SAXPathException;
 
-/** An XPath implementation for the W3C DOM model
+/**
+ * An XPath implementation for the W3C DOM model
+ *
+ * <p>This is the main entry point for matching an XPath against a DOM
+ * tree.  You create a compiled XPath object, then match it against
+ * one or more context nodes using the {@link #selectNodes}
+ * method, as in the following example:</p>
+ *
+ * <pre>
+ * XPath path = new XPath("a/b/c");
+ * List results = path.selectNodes(domContextNode);
+ * </pre>
  *
  * @author <a href="mailto:jstrachan@apache.org">James Strachan</a>
  * @version $Revision$
  */
 public class XPath extends BaseXPath
 {
-    public XPath(String xpathExpr) throws SAXPathException
+
+    /**
+     * Constructor.
+     *
+     * @param xpathExpr A string containing an XPath expression.
+     * @exception SAXPathException If there is a syntactic error in
+     *            the XPath expression.
+     */
+    public XPath(String xpathExpr)
+	throws SAXPathException
     {
         super( xpathExpr );
     }
 
+
+    /**
+     * Generate a navigator object for a DOM tree.
+     *
+     * <p>This method is used internally by the superclass to get
+     * the right kind of navigator.</p>
+     *
+     * @return An instance of a navigator for a DOM2 object tree.
+     */
     protected Navigator getNavigator()
     {
         return DocumentNavigator.getInstance();
     }
+
 } 
+
+// end of XPath.java
