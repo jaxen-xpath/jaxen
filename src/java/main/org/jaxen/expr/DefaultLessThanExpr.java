fcb9e64ad4e17713206a42d6a868340d2b25ccb6
ebolwidt <ebolwidt@43379f7c-b030-0410-81db-e0b70742847c>
2001-09-15 13:22:30 +0000

    Bugfixes; in UnaryExpr, the containing expr wasn't simplified. This gives problems with expressions like "-3" the relational expressions had a bug in the Set-Set comparisions with their inner-loop index variable j the other problem was with the reversal of parameters in String-Set/Set-String and Number-Set/Set-Number comparisons.

--- src/java/main/org/jaxen/expr/DefaultLessThanExpr.java
+++ src/java/main/org/jaxen/expr/DefaultLessThanExpr.java
@@ -37,9 +37,9 @@ class DefaultLessThanExpr extends DefaultRelationalExpr
             lhsStr = StringFunction.evaluate( lhsSet.get( i ),
                                               nav );
 
-            for ( int j = 0 ; j < rhsSetSize ; ++i )
+            for ( int j = 0 ; j < rhsSetSize ; ++j )
             {
-                rhsStr = StringFunction.evaluate( rhsSet.get( i ),
+                rhsStr = StringFunction.evaluate( rhsSet.get( j ),
                                                   nav );
 
                 if ( lhsStr.compareTo( rhsStr ) < 0 )
@@ -65,13 +65,12 @@ class DefaultLessThanExpr extends DefaultRelationalExpr
             setElement = StringFunction.evaluate( theSet.get( i ),
                                                   nav );
 
-            if ( reverse && ( setElement.compareTo( theStr ) > 0 ) )
-            {
-                return Boolean.TRUE;
-            }
-            else if ( setElement.compareTo( theStr ) < 0 )
-            {
-                return Boolean.TRUE;
+            if ( reverse ) {
+                if ( setElement.compareTo( theStr ) > 0 )
+                    return Boolean.TRUE;
+            } else {
+                if ( setElement.compareTo( theStr ) < 0 )
+                    return Boolean.TRUE;
             }
         }
 
@@ -93,13 +92,12 @@ class DefaultLessThanExpr extends DefaultRelationalExpr
             setElement = (Comparable) NumberFunction.evaluate( theSet.get( i ),
                                                                nav );
 
-            if ( reverse && ( setElement.compareTo( theNum ) > 0 ) )
-            {
-                return Boolean.TRUE;
-            }
-            else if ( setElement.compareTo( theNum ) < 0 )
-            {
-                return Boolean.TRUE;
+            if ( reverse ) {
+                if ( setElement.compareTo( theNum ) > 0 )
+                    return Boolean.TRUE;
+            } else {
+                if ( setElement.compareTo( theNum ) < 0 )
+                    return Boolean.TRUE;
             }
         }
 
