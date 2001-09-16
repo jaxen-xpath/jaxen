c0671209040a80ae00c8d57860327bbf348426a1
ebolwidt <ebolwidt@43379f7c-b030-0410-81db-e0b70742847c>
2001-09-16 13:22:22 +0000

    Added handy method to do set namespace context from element in XSLT/XQuery/test-script/whatever document

--- src/java/main/org/jaxen/SimpleNamespaceContext.java
+++ src/java/main/org/jaxen/SimpleNamespaceContext.java
@@ -1,9 +1,14 @@
 
 package org.jaxen;
 
+import java.util.Iterator;
 import java.util.Map;
 import java.util.HashMap;
 
+/**
+ *  Provides mappings from namespace prefix to namespace URI to the xpath
+ *  engine.
+ */
 public class SimpleNamespaceContext implements NamespaceContext
 {
     private Map namespaces;
@@ -11,6 +16,29 @@ public class SimpleNamespaceContext implements NamespaceContext
     public SimpleNamespaceContext()
     {
         this.namespaces = new HashMap();
+    }
+
+    /**
+     *  Adds all the namespace declarations that are in scope on the given
+     *  element. In the case of an XSLT stylesheet, this would be the element
+     *  that has the xpath expression in one of its attributes; i.e.
+     *  <code>&lt;xsl:if test="condition/xpath/expression"&gt;</code>.
+     *
+     *  @param nav  the navigator for use in conjunction with
+     *              <code>element</code>
+     *  @param element  the element to copy the namespaces from
+     */
+    public void addElementNamespaces( Navigator nav, Object element )
+        throws UnsupportedAxisException
+    {
+        Iterator namespaceAxis = nav.getNamespaceAxisIterator( element );
+
+        while ( namespaceAxis.hasNext() ) {
+            Object namespace = namespaceAxis.next();
+            String prefix = nav.getNamespacePrefix( namespace );
+            String uri = nav.getNamespaceStringValue( namespace );
+            addNamespacePrefixTranslation( prefix, uri );
+        }
     }
 
     public void addNamespacePrefixTranslation(String prefix,
