7984698dcb74b381039bfc92a50ea8383479a7d5
ebolwidt <ebolwidt@43379f7c-b030-0410-81db-e0b70742847c>
2001-09-30 11:35:18 +0000

    Added comments and methods for delayed settig of parent element of namespace node.

--- src/java/main/org/jaxen/jdom/XPathNamespace.java
+++ src/java/main/org/jaxen/jdom/XPathNamespace.java
@@ -4,23 +4,53 @@ package org.jaxen.jdom;
 import org.jdom.Element;
 import org.jdom.Namespace;
 
+/** Wrapper for jdom namespace nodes to give them a parent, as required
+ *  by the XPath data model.
+ *
+ *  @author Erwin Bolwidt
+ */
 public class XPathNamespace
 {
     private Element jdomElement;
 
     private Namespace jdomNamespace;
 
+    /** Creates a namespace-node wrapper for a namespace node that hasn't been
+     *  assigned to an element yet.
+     */
+    public XPathNamespace( Namespace jdomNamespace )
+    {
+        this.jdomNamespace = jdomNamespace;
+    }
+
+    /** Creates a namespace-node wrapper for a namespace node that is assigned
+     *  to the given jdom element.
+     */
     public XPathNamespace( Element jdomElement, Namespace jdomNamespace )
     {
         this.jdomElement = jdomElement;
         this.jdomNamespace = jdomNamespace;
     }
 
+    /** Returns the jdom element from which this namespace node has been 
+     *  retrieved. The result may be null when the namespace node has not yet
+     *  been assigned to an element.
+     */
     public Element getJDOMElement()
     {
         return jdomElement;
     }
 
+    /** Sets or changes the element to which this namespace node is assigned.
+     */
+    public void setJDOMElement( Element jdomElement )
+    {
+        this.jdomElement = jdomElement;
+    }
+
+    /** Returns the jdom namespace object of this namespace node; the jdom
+     *  namespace object contains the prefix and uri of the namespace.
+     */
     public Namespace getJDOMNamespace()
     {
         return jdomNamespace;
