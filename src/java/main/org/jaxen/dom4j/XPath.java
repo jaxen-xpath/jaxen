a57eaedd488bc9649ca5ce661805d1306ff1e947
werken <werken@43379f7c-b030-0410-81db-e0b70742847c>
2001-12-11 15:39:54 +0000

    Isolated SAXPath exceptions away from normal Jaxen usage.

--- src/java/main/org/jaxen/dom4j/XPath.java
+++ src/java/main/org/jaxen/dom4j/XPath.java
@@ -2,13 +2,14 @@
 
 package org.jaxen.dom4j;
 
+import org.jaxen.JaxenException;
+
 import org.jaxen.Navigator;
 import org.jaxen.BaseXPath;
-import org.saxpath.SAXPathException;
 
 public class XPath extends BaseXPath
 {
-    public XPath(String xpathExpr) throws SAXPathException
+    public XPath(String xpathExpr) throws JaxenException
     {
         super( xpathExpr );
     }
