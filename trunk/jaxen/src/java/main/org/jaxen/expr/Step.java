ad72fd3f3f632d41e4547d64605ebe71724807e2
slehmann <slehmann@43379f7c-b030-0410-81db-e0b70742847c>
2002-11-11 15:17:55 +0000

    Moved the evaluation of a step into DefaultStep, added javadocs

--- trunk/jaxen/src/java/main/org/jaxen/expr/Step.java
+++ trunk/jaxen/src/java/main/org/jaxen/expr/Step.java
@@ -1,178 +1,115 @@
 /*
-
  * $Header$
-
  * $Revision$
-
  * $Date$
-
  *
-
  * ====================================================================
-
  *
-
  * Copyright (C) 2000-2002 bob mcwhirter & James Strachan.
-
  * All rights reserved.
-
  *
-
  * Redistribution and use in source and binary forms, with or without
-
  * modification, are permitted provided that the following conditions
-
  * are met:
-
  * 
-
  * 1. Redistributions of source code must retain the above copyright
-
  *    notice, this list of conditions, and the following disclaimer.
-
  *
-
  * 2. Redistributions in binary form must reproduce the above copyright
-
  *    notice, this list of conditions, and the disclaimer that follows 
-
  *    these conditions in the documentation and/or other materials 
-
  *    provided with the distribution.
-
  *
-
  * 3. The name "Jaxen" must not be used to endorse or promote products
-
  *    derived from this software without prior written permission.  For
-
  *    written permission, please contact license@jaxen.org.
-
  * 
-
  * 4. Products derived from this software may not be called "Jaxen", nor
-
  *    may "Jaxen" appear in their name, without prior written permission
-
  *    from the Jaxen Project Management (pm@jaxen.org).
-
  * 
-
  * In addition, we request (but do not require) that you include in the 
-
  * end-user documentation provided with the redistribution and/or in the 
-
  * software itself an acknowledgement equivalent to the following:
-
  *     "This product includes software developed by the
-
  *      Jaxen Project (http://www.jaxen.org/)."
-
  * Alternatively, the acknowledgment may be graphical using the logos 
-
  * available at http://www.jaxen.org/
-
  *
-
  * THIS SOFTWARE IS PROVIDED ``AS IS'' AND ANY EXPRESSED OR IMPLIED
-
  * WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
-
  * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
-
  * DISCLAIMED.  IN NO EVENT SHALL THE Jaxen AUTHORS OR THE PROJECT
-
  * CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
-
  * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
-
  * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF
-
  * USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
-
  * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
-
  * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT
-
  * OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
-
  * SUCH DAMAGE.
-
  *
-
  * ====================================================================
-
  * This software consists of voluntary contributions made by many 
-
  * individuals on behalf of the Jaxen Project and was originally 
-
  * created by bob mcwhirter <bob@werken.com> and 
-
  * James Strachan <jstrachan@apache.org>.  For more information on the 
-
  * Jaxen Project, please see <http://www.jaxen.org/>.
-
  * 
-
  * $Id$
-
  */
-
-
-
-
-
-
 
 package org.jaxen.expr;
 
-
-
+import org.jaxen.Context;
 import org.jaxen.ContextSupport;
-
 import org.jaxen.UnsupportedAxisException;
-
-
+import org.jaxen.JaxenException;
 
 import java.util.Iterator;
+import java.util.List;
 
-
-
-//public interface Step extends Predicated, Expr
-
+/**
+ * A Step represents a location step in a LocationPath. The node-set selected by 
+ * the location step is the node-set that results from generating an initial 
+ * node-set from the axis and node-test, and then filtering that node-set by 
+ * each of the predicates in turn.
+ * 
+ * The initial node-set consists of the nodes having the relationship to the 
+ * context node specified by the axis, and having the node type and expanded-name 
+ * specified by the node test.
+ */
 public interface Step extends Predicated, Visitable
-
 {
 
+    /**
+     * Performs the node-test part of evaluating the step for the given node
+     * (which must be on the axis).
+     */    
     boolean matches(Object node,
-
                     ContextSupport contextSupport);
-
-
 
     String getText();
 
     void simplify();
 
-
-
+    /**
+     * Get an identifier for the current axis.
+     * @see org.saxpath.Axis
+     */
     public int getAxis();
 
-    
-
-    /*
-
-    Iterator asIterator(Iterator contextIterator,
-
-                        ContextSupport support);
-
-    */
-
-
-
+    /**
+     * Get an Iterator for the current axis starting in the given contextNode.
+     */    
     Iterator axisIterator(Object contextNode,
-
                           ContextSupport support) throws UnsupportedAxisException;
+
+    /**
+     * For each node in the given context calls matches() for every node on the
+     * axis, then filters the result by each of the predicates.
+     */
+    List evaluate(Context context) throws JaxenException;
 
 }
 
