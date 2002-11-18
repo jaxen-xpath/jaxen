996797ea9e0fd6830d889ffc4aa49b98cb930bc3
szegedia <szegedia@43379f7c-b030-0410-81db-e0b70742847c>
2002-11-18 10:23:50 +0000

    Some more array allocation tuning

--- trunk/jaxen/src/java/main/org/jaxen/function/IdFunction.java
+++ trunk/jaxen/src/java/main/org/jaxen/function/IdFunction.java
@@ -62,6 +62,7 @@
 package org.jaxen.function;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.Iterator;
 import java.util.List;
 import java.util.StringTokenizer;
@@ -92,11 +93,11 @@ public class IdFunction implements Function
 
     public static List evaluate (List contextNodes, Object arg, Navigator nav)
     {
+        if (contextNodes.size() == 0)
+            return Collections.EMPTY_LIST;
+      
         List nodes = new ArrayList();
 
-        if (contextNodes.size() == 0)
-            return nodes;
-      
         Object contextNode = contextNodes.get(0);
 
         if (arg instanceof List) {
