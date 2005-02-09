2b58d9a214c412b0de1ef033903386a8f93a070e
elharo <elharo@43379f7c-b030-0410-81db-e0b70742847c>
2005-02-09 03:23:16 +0000

    Adding test for Jaxen-20

--- trunk/jaxen/src/java/test/org/jaxen/jdom/XPathTest.java
+++ trunk/jaxen/src/java/test/org/jaxen/jdom/XPathTest.java
@@ -70,8 +70,10 @@ import java.util.List;
 
 import org.jaxen.XPath;
 import org.jaxen.saxpath.SAXPathException;
+import org.jdom.Attribute;
 import org.jdom.Document;
 import org.jdom.Element;
+import org.jdom.Namespace;
 import org.jdom.Text;
 import org.jdom.input.SAXBuilder;
 
@@ -189,6 +191,31 @@ public class XPathTest extends TestCase
 
             Text result = (Text) iter.next();
             assertEquals( "1", result.getValue());
+
+        }
+        catch (Exception e)
+        {
+            fail( e.getMessage() );
+        }
+    }
+    
+    public void testJaxen20AttributeNamespaceNodes()
+    {
+        try
+        {
+            Namespace ns1 = Namespace.getNamespace("p1", "www.acme1.org");
+            Namespace ns2 = Namespace.getNamespace("p2", "www.acme2.org");
+            Element element = new Element("test", ns1);
+            Attribute attribute = new Attribute("foo", "bar", ns2);
+            element.setAttribute(attribute); 
+            Document doc = new Document(element);
+            
+            XPath xpath = new JDOMXPath( "//namespace::node()" );
+
+            List results = xpath.selectNodes( doc );
+
+            assertEquals( 3,
+                          results.size() );
 
         }
         catch (Exception e)
