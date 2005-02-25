d9463fe03357bef771d4988847beb5f1da739643
elharo <elharo@43379f7c-b030-0410-81db-e0b70742847c>
2005-02-25 15:00:43 +0000

    Test case for Jaxen-55 Descendant axis returns nodes breadth-first rather than depth-first order

--- trunk/jaxen/src/java/test/org/jaxen/BaseXPathTest.java
+++ trunk/jaxen/src/java/test/org/jaxen/BaseXPathTest.java
@@ -173,4 +173,38 @@ public class BaseXPathTest extends TestCase {
     }    
     
     
+    // test case for JAXEN-55
+    public void testDescendantAxis() throws JaxenException, ParserConfigurationException {
+        
+        BaseXPath xpath = new DOMXPath("//x");
+        DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+        factory.setNamespaceAware(true);
+        org.w3c.dom.Document doc = factory.newDocumentBuilder().newDocument();
+        org.w3c.dom.Element a = doc.createElementNS("", "a");
+        org.w3c.dom.Element b = doc.createElementNS("", "b");
+        doc.appendChild(a);
+        org.w3c.dom.Element x1 = doc.createElementNS("", "x");
+        x1.appendChild(doc.createTextNode("1"));
+        a.appendChild(x1);
+        a.appendChild(b);
+        org.w3c.dom.Element x2 = doc.createElementNS("", "x");
+        org.w3c.dom.Element x3 = doc.createElementNS("", "x");
+        org.w3c.dom.Element x4 = doc.createElementNS("", "x");
+        a.appendChild(x4);
+        b.appendChild(x2);
+        b.appendChild(x3);
+        x2.appendChild(doc.createTextNode("2"));
+        x3.appendChild(doc.createTextNode("3"));
+        x4.appendChild(doc.createTextNode("4"));
+        
+        List result = xpath.selectNodes(doc);
+        assertEquals(4, result.size());
+        assertEquals(x1, result.get(0));   
+        assertEquals(x2, result.get(1));   
+        assertEquals(x3, result.get(2));   
+        assertEquals(x4, result.get(3));
+        
+    }    
+    
+    
 }
