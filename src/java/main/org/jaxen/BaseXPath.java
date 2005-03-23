477c347838e6dd54a5961400873c79b83b404b1e
Elliotte Rusty Harold <elharo@users.noreply.github.com>
2005-03-23 13:12:08 +0000

    JavaDoc improvement

--- src/java/main/org/jaxen/BaseXPath.java
+++ src/java/main/org/jaxen/BaseXPath.java
@@ -338,7 +338,7 @@ public class BaseXPath implements XPath, Serializable
      *
      *  @param node the node, node-set or Context object for evaluation. This value can be null.
      *
-     *  @return the number-value interpretation of this expression
+     *  @return a <code>Double</code> interpretation of this expression
      */
     public Number numberValueOf(Object node) throws JaxenException
     {
@@ -711,6 +711,9 @@ public class BaseXPath implements XPath, Serializable
      */
     protected List selectNodesForContext(Context context) throws JaxenException
     {
+        // FIXME This may no longer be in document order
+        // Before returning this we need to resort into
+        // document order
         return this.xpath.asList( context );
     }
     
