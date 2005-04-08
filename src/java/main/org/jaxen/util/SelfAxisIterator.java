6c488a86d122bea3544c9d7de90c1af773f346f2
Elliotte Rusty Harold <elharo@users.noreply.github.com>
2005-04-08 14:39:13 +0000

    By making SelfAxisIterator extend SingleObjectIterator, we can eliminate several methods

--- src/java/main/org/jaxen/util/SelfAxisIterator.java
+++ src/java/main/org/jaxen/util/SelfAxisIterator.java
@@ -63,40 +63,12 @@
 
 package org.jaxen.util;
 
-import java.util.Iterator;
-import java.util.NoSuchElementException;
-
-public class SelfAxisIterator implements Iterator
+public class SelfAxisIterator extends SingleObjectIterator
 {
-    private boolean hasNext;
-    private Object  node;
 
     public SelfAxisIterator(Object node)
     {
-        this.hasNext = true;
-        this.node    = node;
+        super(node);
     }
-
-    public boolean hasNext()
-    {
-        return this.hasNext;
-    }
-
-    public Object next() throws NoSuchElementException
-    {
-        if ( hasNext() )
-        {
-            this.hasNext = false;
-            return this.node;
-        }
-
-        throw new NoSuchElementException();
-    }
-
-    public void remove() throws UnsupportedOperationException
-    {
-        throw new UnsupportedOperationException();
-    }
-        
 
 }
