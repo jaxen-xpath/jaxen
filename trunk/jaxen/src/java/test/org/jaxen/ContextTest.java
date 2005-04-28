b081c92aa8b4ebe51b3e2573e1926119c6c8d72b
elharo <elharo@43379f7c-b030-0410-81db-e0b70742847c>
2005-04-28 19:20:31 +0000

    Removing unused imports

--- trunk/jaxen/src/java/test/org/jaxen/ContextTest.java
+++ trunk/jaxen/src/java/test/org/jaxen/ContextTest.java
@@ -64,18 +64,15 @@ package org.jaxen;
 
 import junit.framework.TestCase;
 
-import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
-import javax.xml.parsers.ParserConfigurationException;
 
 import org.jaxen.dom.DOMXPath;
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
-import org.xml.sax.SAXException;
 
 public class ContextTest extends TestCase
 {
