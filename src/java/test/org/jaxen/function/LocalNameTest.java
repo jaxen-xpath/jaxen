f5c791ed21fe75450ee38fe199722562c3f0bbc5
Elliotte Rusty Harold <elharo@users.noreply.github.com>
2005-05-01 00:57:32 +0000

    Adding more tests for the local-name function

--- src/java/test/org/jaxen/function/LocalNameTest.java
+++ src/java/test/org/jaxen/function/LocalNameTest.java
@@ -71,9 +71,14 @@ import javax.xml.parsers.ParserConfigurationException;
 import junit.framework.TestCase;
 
 import org.jaxen.FunctionCallException;
+import org.jaxen.JaxenException;
 import org.jaxen.XPath;
 import org.jaxen.dom.DOMXPath;
+import org.w3c.dom.Attr;
+import org.w3c.dom.Comment;
 import org.w3c.dom.Document;
+import org.w3c.dom.ProcessingInstruction;
+import org.w3c.dom.Text;
 import org.xml.sax.SAXException;
 
 /**
@@ -97,23 +102,82 @@ public class LocalNameTest extends TestCase {
         super(name);
     }
 
-    public void testLocalNameOfNumber()
+    public void testLocalNameOfNumber() throws JaxenException
     {
         try
         {
             XPath xpath = new DOMXPath( "local-name(3)" );
             xpath.selectNodes( doc );
             fail("local-name of non-node-set");
-       }
-       catch (FunctionCallException e) 
+        }
+        catch (FunctionCallException e) 
         {
            assertEquals("The argument to the local-name function must be a node-set", e.getMessage());
         }
-       catch (Exception e)
+    }    
+
+    public void testLocalNameWithTwoArguments() throws JaxenException
+    {
+        try
         {
-            e.printStackTrace();
-            fail( e.getMessage() );
+            XPath xpath = new DOMXPath( "local-name(/*, //*)" );
+            xpath.selectNodes( doc );
+            fail("local-name with two arguments");
         }
+        catch (FunctionCallException e) 
+        {
+           assertEquals("local-name() requires zero or one argument.", e.getMessage());
+        }
+    }    
+
+    public void testLocalNameAllowsNoArguments() throws JaxenException
+    {
+        XPath xpath = new DOMXPath( "local-name()" );
+        String result = (String) xpath.evaluate( doc.getDocumentElement() );
+        assertEquals("foo", result);
+    }    
+
+    public void testLocalNameOfCommentIsEmptyString() throws JaxenException
+    {
+        XPath xpath = new DOMXPath( "local-name(/comment())" );
+        Comment c = doc.createComment("test");
+        doc.appendChild(c);
+        String result = (String) xpath.evaluate(doc);
+        assertEquals("", result);
+    }    
+
+    public void testLocalNameOfProcessingInstructionIsTarget() throws JaxenException
+    {
+        XPath xpath = new DOMXPath( "local-name(/processing-instruction())" );
+        ProcessingInstruction pi = doc.createProcessingInstruction("target", "value");
+        doc.appendChild(pi);
+        String result = (String) xpath.evaluate(doc);
+        assertEquals("target", result);
+    }    
+
+    public void testLocalNameOfAttribute() throws JaxenException
+    {
+        XPath xpath = new DOMXPath( "local-name(/*/@*)" );
+        Attr a = doc.createAttribute("name");
+        doc.getDocumentElement().setAttributeNode(a);
+        String result = (String) xpath.evaluate(doc);
+        assertEquals("name", result);
+    }    
+
+    public void testLocalNameOfTextIsEmptyString() throws JaxenException
+    {
+        XPath xpath = new DOMXPath( "local-name(/*/text())" );
+        Text c = doc.createTextNode("test");
+        doc.getDocumentElement().appendChild(c);
+        String result = (String) xpath.evaluate(doc);
+        assertEquals("", result);
+    }    
+
+    public void testLocalNameOfNamespaceIsPrefix() throws JaxenException
+    {
+        XPath xpath = new DOMXPath( "local-name(/*/namespace::node())" );
+        String result = (String) xpath.evaluate(doc);
+        assertEquals("xml", result);
     }    
 
     public void testLocalNameNoArguments()
