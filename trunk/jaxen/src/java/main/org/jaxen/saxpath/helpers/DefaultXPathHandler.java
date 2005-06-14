52b6b137050365feb24bba3a533acd90efeb638c
elharo <elharo@43379f7c-b030-0410-81db-e0b70742847c>
2005-06-14 15:08:30 +0000

    Changing org.jaxen.saxpath.SAXPathException to SAXPathException (i.e. not using fully package qualified classname)

--- trunk/jaxen/src/java/main/org/jaxen/saxpath/helpers/DefaultXPathHandler.java
+++ trunk/jaxen/src/java/main/org/jaxen/saxpath/helpers/DefaultXPathHandler.java
@@ -64,6 +64,7 @@
 
 package org.jaxen.saxpath.helpers;
 
+import org.jaxen.saxpath.SAXPathException;
 import org.jaxen.saxpath.XPathHandler;
 
 /**
@@ -83,182 +84,182 @@ import org.jaxen.saxpath.XPathHandler;
 public class DefaultXPathHandler implements XPathHandler
 {
 
-    public void startXPath() throws org.jaxen.saxpath.SAXPathException
+    public void startXPath() throws SAXPathException
     {
     }
 
-    public void endXPath() throws org.jaxen.saxpath.SAXPathException
+    public void endXPath() throws SAXPathException
     {
     }
 
-    public void startPathExpr() throws org.jaxen.saxpath.SAXPathException
+    public void startPathExpr() throws SAXPathException
     {
     }
 
-    public void endPathExpr() throws org.jaxen.saxpath.SAXPathException
+    public void endPathExpr() throws SAXPathException
     {
     }
 
-    public void startAbsoluteLocationPath() throws org.jaxen.saxpath.SAXPathException
+    public void startAbsoluteLocationPath() throws SAXPathException
     {
     }
 
-    public void endAbsoluteLocationPath() throws org.jaxen.saxpath.SAXPathException
+    public void endAbsoluteLocationPath() throws SAXPathException
     {
     }
 
-    public void startRelativeLocationPath() throws org.jaxen.saxpath.SAXPathException
+    public void startRelativeLocationPath() throws SAXPathException
     {
     }
 
-    public void endRelativeLocationPath() throws org.jaxen.saxpath.SAXPathException
+    public void endRelativeLocationPath() throws SAXPathException
     {
     }
 
     public void startNameStep(int axis,
                               String prefix,
-                              String localName) throws org.jaxen.saxpath.SAXPathException
+                              String localName) throws SAXPathException
     {
     }
 
-    public void endNameStep() throws org.jaxen.saxpath.SAXPathException
+    public void endNameStep() throws SAXPathException
     {
     }
 
-    public void startTextNodeStep(int axis) throws org.jaxen.saxpath.SAXPathException
+    public void startTextNodeStep(int axis) throws SAXPathException
     {
     }
-    public void endTextNodeStep() throws org.jaxen.saxpath.SAXPathException
-    {
-    }
-
-    public void startCommentNodeStep(int axis) throws org.jaxen.saxpath.SAXPathException
+    public void endTextNodeStep() throws SAXPathException
     {
     }
 
-    public void endCommentNodeStep() throws org.jaxen.saxpath.SAXPathException
+    public void startCommentNodeStep(int axis) throws SAXPathException
     {
     }
 
-    public void startAllNodeStep(int axis) throws org.jaxen.saxpath.SAXPathException
+    public void endCommentNodeStep() throws SAXPathException
     {
     }
 
-    public void endAllNodeStep() throws org.jaxen.saxpath.SAXPathException
+    public void startAllNodeStep(int axis) throws SAXPathException
+    {
+    }
+
+    public void endAllNodeStep() throws SAXPathException
     {
     }
 
     public void startProcessingInstructionNodeStep(int axis,
-                                                   String name) throws org.jaxen.saxpath.SAXPathException
+                                                   String name) throws SAXPathException
     {
     }
-    public void endProcessingInstructionNodeStep() throws org.jaxen.saxpath.SAXPathException
-    {
-    }
-
-    public void startPredicate() throws org.jaxen.saxpath.SAXPathException
+    public void endProcessingInstructionNodeStep() throws SAXPathException
     {
     }
 
-    public void endPredicate() throws org.jaxen.saxpath.SAXPathException
+    public void startPredicate() throws SAXPathException
     {
     }
 
-    public void startFilterExpr() throws org.jaxen.saxpath.SAXPathException
+    public void endPredicate() throws SAXPathException
     {
     }
 
-    public void endFilterExpr() throws org.jaxen.saxpath.SAXPathException
+    public void startFilterExpr() throws SAXPathException
     {
     }
 
-    public void startOrExpr() throws org.jaxen.saxpath.SAXPathException
+    public void endFilterExpr() throws SAXPathException
     {
     }
 
-    public void endOrExpr(boolean create) throws org.jaxen.saxpath.SAXPathException
+    public void startOrExpr() throws SAXPathException
     {
     }
 
-    public void startAndExpr() throws org.jaxen.saxpath.SAXPathException
+    public void endOrExpr(boolean create) throws SAXPathException
     {
     }
 
-    public void endAndExpr(boolean create) throws org.jaxen.saxpath.SAXPathException
+    public void startAndExpr() throws SAXPathException
     {
     }
 
-    public void startEqualityExpr() throws org.jaxen.saxpath.SAXPathException
+    public void endAndExpr(boolean create) throws SAXPathException
     {
     }
 
-    public void endEqualityExpr(int operator) throws org.jaxen.saxpath.SAXPathException
+    public void startEqualityExpr() throws SAXPathException
     {
     }
 
-    public void startRelationalExpr() throws org.jaxen.saxpath.SAXPathException
+    public void endEqualityExpr(int operator) throws SAXPathException
     {
     }
 
-    public void endRelationalExpr(int operator) throws org.jaxen.saxpath.SAXPathException
+    public void startRelationalExpr() throws SAXPathException
     {
     }
 
-    public void startAdditiveExpr() throws org.jaxen.saxpath.SAXPathException
+    public void endRelationalExpr(int operator) throws SAXPathException
     {
     }
 
-    public void endAdditiveExpr(int operator) throws org.jaxen.saxpath.SAXPathException
+    public void startAdditiveExpr() throws SAXPathException
     {
     }
 
-    public void startMultiplicativeExpr() throws org.jaxen.saxpath.SAXPathException
+    public void endAdditiveExpr(int operator) throws SAXPathException
     {
     }
 
-    public void endMultiplicativeExpr(int operator) throws org.jaxen.saxpath.SAXPathException
+    public void startMultiplicativeExpr() throws SAXPathException
     {
     }
 
-    public void startUnaryExpr() throws org.jaxen.saxpath.SAXPathException
+    public void endMultiplicativeExpr(int operator) throws SAXPathException
     {
     }
 
-    public void endUnaryExpr(int operator) throws org.jaxen.saxpath.SAXPathException
+    public void startUnaryExpr() throws SAXPathException
     {
     }
 
-    public void startUnionExpr() throws org.jaxen.saxpath.SAXPathException
+    public void endUnaryExpr(int operator) throws SAXPathException
     {
     }
 
-    public void endUnionExpr(boolean create) throws org.jaxen.saxpath.SAXPathException
+    public void startUnionExpr() throws SAXPathException
     {
     }
 
-    public void number(int number) throws org.jaxen.saxpath.SAXPathException
+    public void endUnionExpr(boolean create) throws SAXPathException
     {
     }
 
-    public void number(double number) throws org.jaxen.saxpath.SAXPathException
+    public void number(int number) throws SAXPathException
     {
     }
 
-    public void literal(String literal) throws org.jaxen.saxpath.SAXPathException
+    public void number(double number) throws SAXPathException
+    {
+    }
+
+    public void literal(String literal) throws SAXPathException
     {
     }
 
     public void variableReference(String prefix,
-                                  String variableName) throws org.jaxen.saxpath.SAXPathException
+                                  String variableName) throws SAXPathException
     {
     }
 
     public void startFunction(String prefix,
-                              String functionName) throws org.jaxen.saxpath.SAXPathException
+                              String functionName) throws SAXPathException
     {
     }
 
-    public void endFunction() throws org.jaxen.saxpath.SAXPathException
+    public void endFunction() throws SAXPathException
     {
     }
 
