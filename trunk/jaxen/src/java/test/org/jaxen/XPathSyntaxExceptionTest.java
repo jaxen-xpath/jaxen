4a1b4cf0fd8e5e510f8781e9eeff5cfd1f786a3a
elharo <elharo@43379f7c-b030-0410-81db-e0b70742847c>
2005-06-19 19:35:25 +0000

    Adding tests for XPathSyntaxException

--- trunk/jaxen/src/java/test/org/jaxen/XPathSyntaxExceptionTest.java
+++ trunk/jaxen/src/java/test/org/jaxen/XPathSyntaxExceptionTest.java
@@ -5,7 +5,7 @@
  *
  * ====================================================================
  *
- * Copyright (C) 2005 bob mcwhirter & James Strachan.
+ * Copyright (C) 2005 Elliotte Rusty Harold.
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
@@ -59,47 +59,59 @@
  * $Id$
  */
 
-
 package org.jaxen;
 
-import junit.framework.Test;
+
 import junit.framework.TestCase;
-import junit.framework.TestSuite;
+import org.jaxen.dom.*;
 
 /**
- * <p>
- *   Collect the org.jaxen. tests.
- * </p>
- * 
  * @author Elliotte Rusty Harold
- * @version 1.1b7
  *
  */
-public class CoreTests extends TestCase {
+public class XPathSyntaxExceptionTest extends TestCase {
 
-    
-    public CoreTests(String name) {
-        super(name);   
+    public XPathSyntaxExceptionTest(String name) {
+        super(name);
     }
 
-    
-    public static Test suite() {
+    public void testGetXPath() throws JaxenException {
         
-        TestSuite result = new TestSuite();
-        result.addTest(new TestSuite(AddNamespaceTest.class));
-        result.addTest(new TestSuite(BaseXPathTest.class));
-        result.addTest(new TestSuite(FunctionContextTest.class));
-        result.addTest(new TestSuite(ContextTest.class));
-        result.addTest(new TestSuite(JaxenHandlerTest.class));
-        result.addTest(new TestSuite(JaxenRuntimeExceptionTest.class));
-        result.addTest(new TestSuite(FunctionCallExceptionTest.class));
-        result.addTest(new TestSuite(UnresolvableExceptionTest.class));
-        result.addTest(new TestSuite(VariableContextTest.class));
-        result.addTest(new TestSuite(SimpleNamespaceContextTest.class));
-        result.addTest(new TestSuite(XPathSyntaxExceptionTest.class));
-        return result;
+        try {
+            new DOMXPath("///triple slash");
+            fail("Bad parsing");
+        }
+        catch (XPathSyntaxException ex) {
+            assertEquals("///triple slash", ex.getXPath());
+        }
         
-    }
+    }    
+  
+    public void testGetPositionMarker() throws JaxenException {
+        
+        try {
+            new DOMXPath("///triple slash");
+            fail("Bad parsing");
+        }
+        catch (XPathSyntaxException ex) {
+            assertTrue(ex.getPositionMarker().startsWith("  "));
+            assertTrue(ex.getPositionMarker().endsWith("^"));
+        }
+        
+    }    
 
-    
+    public void testGetMultilineMessage() throws JaxenException {
+        
+        try {
+            new DOMXPath("///triple slash");
+            fail("Bad parsing");
+        }
+        catch (XPathSyntaxException ex) {
+            String message = ex.getMultilineMessage();
+            assertTrue(message.indexOf("\n///triple slash\n") > 1);
+            assertTrue(message.endsWith("^"));
+        }
+        
+    }    
+
 }
