a6d4f9ed1bdf51d42bf4cca25bb8b2e712a8dbea
Elliotte Rusty Harold <elharo@users.noreply.github.com>
2005-06-19 14:08:55 +0000

    Avoiding exception when negative lengths are passed into substring function; instead returning empty string as required by XPath spec

--- src/java/main/org/jaxen/function/SubstringFunction.java
+++ src/java/main/org/jaxen/function/SubstringFunction.java
@@ -103,24 +103,27 @@ public class SubstringFunction implements Function
         // Round the value and subtract 1 as Java strings are zero based
         int start = RoundFunction.evaluate(d1, nav).intValue() - 1;
 
-        int len = strlen;
+        int length = strlen;
         if (argc == 3){
             Double d2 = NumberFunction.evaluate(args.get(2), nav);
 
             if (!d2.isNaN()){
-                len = RoundFunction.evaluate(d2, nav ).intValue();
+                length = RoundFunction.evaluate(d2, nav ).intValue();
             }
             else {
-                len = 0;
+                length = 0;
             }
         }
 
-        int end = start + len;
+        if (length < 0) return "";
+        
+        int end = start + length;
 
         // negative start is treated as 0
         if ( start < 0){
             start = 0;
-        }else if (start > strlen){
+        }
+        else if (start > strlen){
             return "";
         }
 
