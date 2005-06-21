5eb9ade228ed368c19a9251dfacd16f67130b3e5
elharo <elharo@43379f7c-b030-0410-81db-e0b70742847c>
2005-06-21 14:06:44 +0000

    Should use a XOMXPath rather than a JDOMXPath

--- trunk/jaxen/src/java/test/org/jaxen/xom/Performance.java
+++ trunk/jaxen/src/java/test/org/jaxen/xom/Performance.java
@@ -47,7 +47,6 @@ package org.jaxen.xom;
 
 import java.io.File;
 
-import org.jaxen.jdom.JDOMXPath;
 import nu.xom.Document;
 import nu.xom.Element;
 import nu.xom.Builder;
@@ -57,7 +56,7 @@ class Performance {
     public static void main(String[] args) {
         try {
             Document doc = new Builder().build(new File("D:/dev/jaxen/xml/much_ado.xml"));
-            JDOMXPath xpath = new JDOMXPath("PLAY/ACT/SCENE/SPEECH/SPEAKER");
+            XOMXPath xpath = new XOMXPath("PLAY/ACT/SCENE/SPEECH/SPEAKER");
             
             long start = System.currentTimeMillis();
             
