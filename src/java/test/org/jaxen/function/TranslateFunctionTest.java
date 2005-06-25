d9a4c458f3012094d6c50b4e8a901691ffc76098
Elliotte Rusty Harold <elharo@users.noreply.github.com>
2005-06-25 15:40:40 +0000

    Adding basic tests for translate function

--- src/java/test/org/jaxen/function/TranslateFunctionTest.java
+++ src/java/test/org/jaxen/function/TranslateFunctionTest.java
@@ -5,7 +5,7 @@
  *
  * ====================================================================
  *
- * Copyright (C) 2005 bob mcwhirter & James Strachan.
+ * Copyright (C) 2005 Elliotte Rusty Harold.
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
@@ -59,62 +59,72 @@
  * $Id$
  */
 
-
 package org.jaxen.function;
 
-import junit.framework.Test;
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+
 import junit.framework.TestCase;
-import junit.framework.TestSuite;
+
+import org.jaxen.FunctionCallException;
+import org.jaxen.JaxenException;
+import org.jaxen.XPath;
+import org.jaxen.dom.DOMXPath;
+import org.w3c.dom.Document;
 
 /**
- * <p>
- *   Suite for Jaxen's function tests.
- * </p>
- * 
  * @author Elliotte Rusty Harold
- * @version 1.1b4
  *
  */
-public class FunctionTests extends TestCase {
+public class TranslateFunctionTest extends TestCase {
 
+    private Document doc;
     
-    public FunctionTests(String name) {
-        super(name);   
+    public void setUp() throws ParserConfigurationException
+    {
+        DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+        factory.setNamespaceAware(true);
+        DocumentBuilder builder = factory.newDocumentBuilder();
+        doc = builder.newDocument();
+        doc.appendChild(doc.createElement("root"));
     }
 
-    
-    public static Test suite() {
+    public void testTranslate() throws JaxenException
+    {
+        XPath xpath = new DOMXPath( "translate('abc', 'b', 'd')" );
+        String result = (String) xpath.evaluate( doc );
+        assertEquals("adc", result);
+    }    
+  
+    public void testSubstringAfterFunctionRequiresAtLeastThreeArguments() 
+      throws JaxenException {
         
-        TestSuite result = new TestSuite();
-        result.addTest(new TestSuite(TranslateFunctionTest.class));
-        result.addTest(new TestSuite(SubstringTest.class));
-        result.addTest(new TestSuite(SubstringBeforeTest.class));
-        result.addTest(new TestSuite(SubstringAfterTest.class));
-        result.addTest(new TestSuite(LangTest.class));
-        result.addTest(new TestSuite(LastTest.class));
-        result.addTest(new TestSuite(ConcatTest.class));
-        result.addTest(new TestSuite(ContainsTest.class));
-        result.addTest(new TestSuite(StringLengthTest.class));
-        result.addTest(new TestSuite(StartsWithTest.class));
-        result.addTest(new TestSuite(CountTest.class));
-        result.addTest(new TestSuite(LocalNameTest.class));
-        result.addTest(new TestSuite(NameTest.class));
-        result.addTest(new TestSuite(NamespaceURITest.class));
-        result.addTest(new TestSuite(SumTest.class));
-        result.addTest(new TestSuite(NumberTest.class));
-        result.addTest(new TestSuite(RoundTest.class));
-        result.addTest(new TestSuite(StringTest.class));
-        result.addTest(new TestSuite(BooleanTest.class));
-        result.addTest(new TestSuite(CeilingTest.class));
-        result.addTest(new TestSuite(FloorTest.class));
-        result.addTest(new TestSuite(IdTest.class));
-        result.addTest(new TestSuite(TrueTest.class));
-        result.addTest(new TestSuite(FalseTest.class));
-        result.addTest(new TestSuite(NotTest.class));
-        result.addTest(new TestSuite(NormalizeSpaceTest.class));
-        return result;
+        XPath xpath = new DOMXPath("translate('a', 'b')");
         
-    }
+        try {
+            xpath.selectNodes(doc);
+            fail("Allowed translate function with two arguments");
+        }
+        catch (FunctionCallException ex) {
+            assertNotNull(ex.getMessage());
+        }
+        
+    }    
 
-    
+    public void testSubstringAfterFunctionRequiresAtMostThreeArguments() 
+      throws JaxenException {
+        
+        XPath xpath = new DOMXPath("substring-after('a', 'a', 'a', 'a')");
+        
+        try {
+            xpath.selectNodes(doc);
+            fail("Allowed translate function with four arguments");
+        }
+        catch (FunctionCallException ex) {
+            assertNotNull(ex.getMessage());
+        }
+        
+    }    
+
 }
