4b166d67d192e37322d27a608b2e1df83b0744d2
Elliotte Rusty Harold <elharo@users.noreply.github.com>
2006-04-08 15:02:30 +0000

    Correcting spelling error in comment

--- src/java/test/org/jaxen/test/XPathTestBase.java
+++ src/java/test/org/jaxen/test/XPathTestBase.java
@@ -1527,8 +1527,8 @@ public abstract class XPathTestBase extends TestCase
         XPath contextPath = new BaseXPath("/p/text()", nav);
         log("Initial Context :: " + contextPath);
         List list = contextPath.selectNodes(document);
-        // Depending on the object model there can be anywhere from 
-        // 1 to 3 nodes returend here
+        // Depending on the object model, there can be anywhere from 
+        // 1 to 3 nodes returned here.
         StringBuffer buffer = new StringBuffer(10);
         Iterator iterator = list.iterator();
         while (iterator.hasNext()) {
