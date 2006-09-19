965c24a8edb6407d157f2ea4b30d619e5ee487f3
elharo <elharo@43379f7c-b030-0410-81db-e0b70742847c>
2006-09-19 19:41:17 +0000

    Adding test for Jaxen-158

--- trunk/jaxen/src/java/test/org/jaxen/test/JDOMNavigatorTest.java
+++ trunk/jaxen/src/java/test/org/jaxen/test/JDOMNavigatorTest.java
@@ -48,8 +48,16 @@
 
 package org.jaxen.test;
 
+import java.util.List;
+
+import org.jaxen.JaxenException;
 import org.jaxen.Navigator;
+import org.jaxen.XPath;
 import org.jaxen.jdom.DocumentNavigator;
+import org.jaxen.jdom.JDOMXPath;
+import org.jdom.Document;
+import org.jdom.Element;
+import org.jdom.Namespace;
 import org.jdom.input.SAXBuilder;
 
 public class JDOMNavigatorTest extends XPathTestBase
@@ -70,4 +78,18 @@ public class JDOMNavigatorTest extends XPathTestBase
     {
         return this.builder.build( url );
     }
+    
+    
+    public void testNullNamespace() throws JaxenException {
+
+        Namespace my = Namespace.getNamespace("foo", "http://mynamespace.org/");
+        Document doc = new Document();
+        Element root = new Element("root", my);
+        doc.setRootElement(root);
+        XPath nullNamespacePath = new JDOMXPath("/root");
+        List selectedNodes = nullNamespacePath.selectNodes(doc);
+        assertEquals(0, selectedNodes.size());
+        
+    }
+    
 }
