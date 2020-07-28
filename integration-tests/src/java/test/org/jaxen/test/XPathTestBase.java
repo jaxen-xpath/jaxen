898eb1de01e95518d95443e4a9b0e8ecac80070d
Paul Duffin <paulduffin@google.com>
2020-07-28 16:39:44 +0100

    Use more inclusive language
    Replace dummy with fake.

--- integration-tests/src/java/test/org/jaxen/test/XPathTestBase.java
+++ integration-tests/src/java/test/org/jaxen/test/XPathTestBase.java
@@ -1619,13 +1619,13 @@ public abstract class XPathTestBase extends TestCase
         log("Initial Context :: " + contextpath);
         List list = contextpath.selectNodes(document);
         SimpleNamespaceContext nsContext = new SimpleNamespaceContext();
-        nsContext.addNamespace("dummy", "http://dummyNamespace/");
+        nsContext.addNamespace("fake", "http://fakeNamespace/");
         getContextSupport().setNamespaceContext(nsContext);
         Iterator iter = list.iterator();
         while (iter.hasNext())
         {
             Object context = iter.next();
-            assertCountXPath(1, context, "/dummy:a/dummy:b/dummy:c");
+            assertCountXPath(1, context, "/fake:a/fake:b/fake:c");
         }
     }
 
