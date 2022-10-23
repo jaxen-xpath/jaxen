f71072110ec98a0aef363d8f30b68d3819b537de
Elliotte Rusty Harold <elharo@users.noreply.github.com>
2022-10-23 16:47:49 +0000

    Try retaining navigators but marking dependencies optional (#127)
    * Revert "remove xom navigator (#122)"
    
    This reverts commit 43aebff5ea3b4b6d995e80df0a8c5bf50d965b1b.
    
    * Revert "remove dom4j (#120)"
    
    This reverts commit 84c5f99fbff7190e573aa3059b8a8259bd247e04.
    
    * Revert "remove jdom (#121)"
    
    This reverts commit 761a48f80a09ecda18860e93e9b3e900924203ab.
    
    * move submodules back to core and make optional
    
    * delete submodule xom

---
+++ core/src/java/test/org/jaxen/test/XOMPerformance.java
@@ -0,0 +1,67 @@
+/*
+ $Id$
+
+ Copyright 2003 The Werken Company. All Rights Reserved.
+ 
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions are
+met:
+
+  * Redistributions of source code must retain the above copyright
+    notice, this list of conditions and the following disclaimer.
+
+  * Redistributions in binary form must reproduce the above copyright
+    notice, this list of conditions and the following disclaimer in the
+    documentation and/or other materials provided with the distribution.
+
+  * Neither the name of the Jaxen Project nor the names of its
+    contributors may be used to endorse or promote products derived 
+    from this software without specific prior written permission.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS
+IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED
+TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A
+PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER
+OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+ */
+package org.jaxen.test;
+
+import org.jaxen.xom.XOMXPath;
+
+import nu.xom.Document;
+import nu.xom.Element;
+import nu.xom.Builder;
+
+class XOMPerformance {
+    
+    public static void main(String[] args) {
+        
+        try {
+            Document doc = new Builder().build("http://www.ibiblio.org/xml/examples/shakespeare/much_ado.xml");
+            XOMXPath xpath = new XOMXPath("PLAY/ACT/SCENE/SPEECH/SPEAKER");
+            
+            long start = System.currentTimeMillis();
+            
+            int count = 0;
+            for (int i = 0; i < 1000; i++) {
+                Element speaker = (Element) xpath.selectSingleNode(doc);
+                count += (speaker == null ? 0 : 1);
+            }
+            
+            long end = System.currentTimeMillis();
+            System.out.println((end - start));
+            System.out.println(count);
+            
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+    }
+    
+}
